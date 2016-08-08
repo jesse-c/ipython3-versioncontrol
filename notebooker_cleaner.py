82e6d48f3990cdbd09346b250077db54c259c3ec
Jesse Claven <j.claven@elsevier.com>
2016-08-08 12:56:03 +0100

    Refactor to take in an option to either overwrite the file or write to a new file.

--- notebooker_cleaner.py
+++ notebooker_cleaner.py
@@ -1,40 +1,58 @@
+"""Clean IPython3 (Jupter, nbformat 4) notebooks.
+
+Usage:
+  notebook_cleaner -o <notebook>
+  notebook_cleaner -f <output> <notebook>
+  notebook_cleaner -v | --version
+  notebook_cleaner -h | --help
+
+Options:
+  -o            Overwrite file in place.
+  -f <file>     Write to specified file.
+  -h --help     Show this screen.
+  -v --version  Show version.
+"""
+
 from docopt import docopt
 import sys
 import os.path
 import json
 from collections import OrderedDict
 
-assert len(sys.argv) == 2,             \
-  "Missing file path arg."
-assert os.path.isfile(sys.argv[1]),    \
-    "File path arg isn't a file."
-assert sys.argv[1].endswith(".ipynb"), \
-  "The file wasn't an IPython notebook (.ipynb)."
 
-with open(sys.argv[1], 'r+') as f:
-    # Read in the notebook and retain order
-    j = json.loads(f.read(), object_pairs_hook=OrderedDict)
+def clean(notebook, output):
+    with open(notebook, 'r+') as f:
+        # Read in the notebook and retain order
+        j = json.loads(f.read(), object_pairs_hook=OrderedDict)
 
-    # Reset trivial changes
-    for i in range(len(j['cells'])):
-        if j['cells'][i]['cell_type'] == 'code':
-            j['cells'][i]['execution_count'] = None
-            j['cells'][i]['outputs'] = []
-            j['cells'][i]['metadata'] = {}
-        elif j['cells'][i]['cell_type'] == 'markdown':
-            j['cells'][i]['metadata'] = {}
+        # Reset trivial changes
+        for i in range(len(j['cells'])):
+            if j['cells'][i]['cell_type'] == 'code':
+                j['cells'][i]['execution_count'] = None
+                j['cells'][i]['outputs'] = []
+                j['cells'][i]['metadata'] = {}
+            elif j['cells'][i]['cell_type'] == 'markdown':
+                j['cells'][i]['metadata'] = {}
 
-    # Construct the filename for the clean version
-    base = os.path.basename(sys.argv[1])
-    base_split = os.path.splitext(base)
-    clean = '{}.clean{}'.format(base_split[0], base_split[1])
-    file_dir = os.path.dirname(sys.argv[1])
-    if file_dir:
-        clean = '{}/{}'.format(file_dir, clean)
+        with open(output, 'w+') as s:
+            print('Writing clean version to {}'.format(output))
+            # Dump the JSON
+            json.dump(j, s, indent=1, separators=(',', ': '))
+            # Add a newline char to prevent a diff at EOF
+            s.write('\n')
 
-    with open(clean, 'w+') as s:
-        print('Writing clean version to {}'.format(clean))
-        # Dump the JSON
-        json.dump(j, s, indent=1, separators=(',', ': '))
-        # Add a newline char to prevent a diff at EOF
-        s.write('\n')
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='notebook_cleaner 0.0.1')
+
+    notebook = arguments['<notebook>']
+    output = arguments['-f']
+
+    assert os.path.isfile(notebook),    \
+        "Notebook path arg isn't a file."
+    assert notebook.endswith(".ipynb"), \
+        "The file wasn't an IPython notebook (.ipynb)."
+
+    if arguments['-o']:
+        clean(notebook, notebook)
+    elif arguments['-f']:
+        clean(notebook, output)
